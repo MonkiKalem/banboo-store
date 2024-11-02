ce9eeb8754203d10f08747a7b01f10eb4aeb73ba
zakysatya <zakysatyakuthan@gmail.com>
2024-11-02 23:36:33 +0700

    First

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final primaryColor = const Color.fromRGBO(35, 12, 15, 1);
+  final secondaryColor = const Color.fromRGBO(234, 214, 55, 1);
+
+  final adminUsername = const Text("admin");
+  final adminPassword = const Text("admin");
+
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Constants _constants = Constants();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+
+              children: [
+              ]
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+}
